976fab7a782769beec409e847012d3576ed0f053
Miquel <54858840+Miquellama@users.noreply.github.com>
2023-08-02 19:28:21 +0200

    Update cuadrado.py

--- cuadrado.py
+++ cuadrado.py
@@ -1,7 +1,7 @@
 import time
 import os
 
-def borrarPantalla(): #Definimos la función estableciendo el nombre que queramos
+def borrarPantalla():
     if os.name == "posix":
        os.system ("clear")
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
